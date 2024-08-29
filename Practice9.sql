19b711299a9d5e4af8ea0758e60c90aad192f478
cansunakipoglu <cansuunakipoglu@gmail.com>
2024-08-29 23:06:44 +0300

    Practice9 Add

---
+++ Practice9.sql
@@ -0,0 +1,6 @@
+SELECT city.city, country.country country FROM city LEFT JOIN country ON city.country_id = country.country_id;
+
+SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer FULL JOIN rental ON customer.customer_id = rental.rental_id; 
+
